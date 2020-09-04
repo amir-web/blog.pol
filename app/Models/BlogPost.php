3627c717b75f7481b496e4626190450480b2c731
AbubakrYusupov <63547836+AbubakrYusupov@users.noreply.github.com>
2020-09-04 18:12:53 +0500

    выводили статьи, написали маршруты

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -3,8 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BlogPost extends Model
 {
-    //
+    use SoftDeletes;/* это мы подключаем когда используем deleted_at */
 }
